a99551546286ce17b95a957c3b9abf47f1716aa0
Charlotte Gunn Hunt <charlotte.gunn@kellogg.northwestern.edu>
2023-04-13 15:50:29 +0000

    Practice work completed by CGH on 4.13.23

--- practice-6.sql
+++ practice-6.sql
@@ -16,3 +16,7 @@
 -- | Burnside               | 10       |
 
 
+SELECT listings.neighborhood, COUNT (reviews.id)
+FROM listings INNER JOIN reviews
+ON listings.id = listing_id
+GROUP BY listings.neighborhood;
